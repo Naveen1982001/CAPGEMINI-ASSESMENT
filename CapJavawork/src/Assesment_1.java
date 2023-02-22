dfa5db7a8f552cb15fb151ff014009f8a9f0760c
Naveen Depp <Naveen Depp@DESKTOP-77J411G>
2023-02-22 12:43:39 +0530

    Assesment 1

---
+++ CapJavawork/src/Assesment_1.java
@@ -0,0 +1,27 @@
+public class Assesment_1 {
+
+	public static void main(String[] args) {
+		// float
+		float maxfloat=3.4028235E38F;
+		float minfloat=-3.4028235E38F;
+		
+		System.out.println(maxfloat);
+		System.out.println(minfloat);
+		
+		// double 
+		double maxdouble =1.7976931348623157E308;
+		double mindouble =-1.7976931348623157E308;
+		
+		System.out.println(maxdouble);
+		System.out.println(mindouble);
+        
+		//boolean
+		
+		boolean boolTrue=true;
+		boolean boolFalse=false;
+		
+		System.out.println(boolTrue);
+		System.out.println(boolFalse);
+	}
+
+}
