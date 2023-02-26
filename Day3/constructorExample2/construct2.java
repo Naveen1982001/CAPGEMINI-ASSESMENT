ecfc8e94bc2d07766565118883f21c3168813a06
Naveen Depp <Naveen Depp@DESKTOP-77J411G>
2023-02-26 12:20:44 +0530

    day3 constructor

---
+++ Day3/constructorExample2/construct2.java
@@ -0,0 +1,14 @@
+
+public class construct2 {
+
+	public static void main(String[] args) {
+		customerdemo2 c1= new customerdemo2 (1,"navee","ch");
+		
+		customerdemo2 c2=new customerdemo2(0,"null","null");
+		c2.display();
+		
+	
+
+	}
+
+}
